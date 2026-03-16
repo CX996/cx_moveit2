9b6661f552b9d919c7812b99d0d40b2df374689f
CX996 <505381348@qq.com>
2026-03-16 13:15:31 +0800

    1. 完成测试

--- ws_moveit/src/cr7_robot_controller/src/planners/ompl/cr7_ompl_planner.cpp
+++ ws_moveit/src/cr7_robot_controller/src/planners/ompl/cr7_ompl_planner.cpp
@@ -1010,7 +1010,8 @@ CR7BaseController::Result CR7OMPLPlanner::moveToPoseImpl(
             ss << "Ompl_trajectory_" << std::put_time(std::localtime(&time_t), "%Y%m%d_%H%M%S") << "_";
             std::string trajectory_prefix = ss.str();
             
-            cr7_controller::utils::TrajectoryAnalyzer::saveDetailedTrajectoryAnalysis(plan.trajectory_, trajectory_prefix, logger_);
+            cr7_controller::utils::TrajectoryAnalyzer::saveDetailedTrajectoryAnalysis(
+                            plan.trajectory_, trajectory_prefix, move_group_, logger_);
         }
         else 
         {
